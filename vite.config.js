a5d7ebcc73ff944123e05456531577c82256f541
Shoaib Ahmed <shoaibah432@gmail.com>
2025-05-01 18:17:15 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/shoaibah432.github.io/' : '/',
+  base: '/shoaibah432.github.io/',
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'], // Ensure .jsx is included
   },
-}))
+})
